c5043fee109b6650cb3fdec675346fbaa514f180
Olivier Chauvel <olchauvel@gmail.com>
2011-09-29 12:03:14 +0200

    Modify css

--- Resources/public/js/jquery.collection.js
+++ Resources/public/js/jquery.collection.js
@@ -7,60 +7,65 @@
  * file that was distributed with this source code.
  */
  (function($) {
-    var $this = null;
+    var params = {
+        i18n: {
+            add: 'Add collection',
+            edit: 'Edit',
+            remove: 'Remove'
+        }
+    };
 
     var methods = {
-        add_collection: function(el) {
+        add_collection: function (el) {
             index = el.children().length;
+            node = $(el.attr('data-prototype').replace(/\$\$name\$\$/g, index));
 
-            el.append(
-                $(el.attr('data-prototype').replace(/\$\$name\$\$/g, index))
-                    .children()
-                        .hide()
-                        .children()
-                            .filter('#'+el.attr('id')+'_'+index+'_ordering')
-                                .val(index)
-                            .end()
-                        .end()
-                    .end()
-                    .prepend($('<div>', {'class': 'bandeau'}).append(
-                        $('<div>', {'class': 'description'}).html('Description'),
-                        $('<div>', {'class': 'actions'}).append(
-                            $('<button>', {'type': 'button', 'class': 'edit'}).html('Edit').click(function() {
-                                $(this).parents('.bandeau').siblings('div').toggle();
-                            }),
-                            $('<button>', {'type': 'button', 'class': 'delete'}).html('Delete').click(function() {
-                                $(this).parents('.bandeau').parent().remove();
-                            })
-                        )
-                    ))
-            );
-        }
-    }
+            el.append(node);
+            methods.create_bandeau(node.children().filter('div'));
+        },
+        create_bandeau: function (el) {
+            el.hide().prev().hide();
 
-    $.fn.genemuCollection = function() {
-        $this = $(this);
+            bandeau = $('<div>', {'class': 'description'});
+            el.find('label').each(function () {
+                title = $(this).text();
+                value = $(this).next().val();
 
-        $this
-            .children()
-            .children().hide().end()
-            .prepend($('<div>', {'class': 'bandeau'}).append(
-                $('<div>', {'class': 'description'}).html('Description'),
+                bandeau.append($('<p>').html('<p><strong>'+title+':</strong> '+value+'</p>'));
+            });
+
+            el.parent().prepend($('<div>', {'class': 'bandeau'}).append(
+                bandeau,
                 $('<div>', {'class': 'actions'}).append(
-                    $('<button>', {'type': 'button', 'class': 'edit'}).html('Edit').click(function() {
+                    $('<button>', {'type': 'button', 'class': 'edit'}).html(params.i18n.edit).click(function() {
                         $(this).parents('.bandeau').siblings('div').toggle();
                     }),
-                    $('<button>', {'type': 'button', 'class': 'delete'}).html('Delete').click(function() {
+                    $('<button>', {'type': 'button', 'class': 'delete'}).html(params.i18n.remove).click(function() {
                         $(this).parents('.bandeau').parent().remove();
                     })
                 )
             ));
+        },
+        init: function (el) {
+            nodes = el.children().children();
 
-        $this.after(
-            $('<button>', {'type': 'button'}).html('Add collection').click(function() {
-                methods.add_collection(jQuery(this).prev());
-            })
-        );
+            nodes.filter('div').each(function () {
+                methods.create_bandeau($(this));
+            });
+
+            el.after(
+                $('<button>', {'type': 'button'}).html(params.i18n.add).click(function() {
+                    methods.add_collection(jQuery(this).prev());
+                })
+            );
+        }
+    }
+
+    $.fn.genemuCollection = function(options) {
+        params = $.extend(params, options);
+
+        $this = $(this);
+        methods.init($this);
 
         return $this;
     };
